faecfdfe7f2dfe29fc8f4a65be239cceb7c90443
Florian Schiessl <fschiessl@fqs.pl>
2022-06-12 10:44:58 +0200

    initialized spring boot project digitalwallet

---
+++ digitalwallet/src/test/java/com/oepfelbaum/techassessment/digitalwallet/DigitalwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oepfelbaum.techassessment.digitalwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
